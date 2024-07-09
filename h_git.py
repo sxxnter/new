ab315327dd9a2e4f4e3d96464a379bb95d253414
sAnter <sxxnter@gmail.com>
2024-07-10 00:44:41 +0500

    made other changes

--- h_git.py
+++ h_git.py
@@ -1,5 +1,6 @@
 def returner(a):
     return a**a
 
-print(list(map(returner, [1, 2, 3, 4, 5])))
-print(list(map(returner, [1, 2, 3, 4, 5])))
+my_list = list(map(returner, [1, 2, 3, 4, 5]))
+my_list.reverse()
+print(my_list)
