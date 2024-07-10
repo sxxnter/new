532d0fa9da928684c3d3276143db5cbaac1e04a1
sAnter <sxxnter@gmail.com>
2024-07-10 23:29:32 +0500

    one more

--- hello_git.py
+++ hello_git.py
@@ -6,3 +6,8 @@ print(f"{name}, will you be my friend?")
 
 name = input("Enter your name ")
 print(f"{name}, will you be my friend?")
+
+data = [1, 2, 3, 4, 5]
+while (n := len(data)) > 0:
+    print(f'Got {n} items')
+    data.pop()
