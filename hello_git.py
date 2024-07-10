e1201168ef6641feb3b0ba8450a20b599f32c0c1
sAnter <sxxnter@gmail.com>
2024-07-10 23:28:50 +0500

    mammm

--- hello_git.py
+++ hello_git.py
@@ -1,8 +1,2 @@
 name = input("Enter your name ")
 print(f"{name}, will you be my friend?")
-
-name = input("Enter your name ")
-print(f"{name}, will you be my friend?")
-
-name = input("Enter your name ")
-print(f"{name}, will you be my friend?")
--- why.py
+++ why.py
@@ -1,6 +1,5 @@
 s = list(map(lambda x: x*x, [1, 2, 3, 4, 5]))
 ss = ' '.join(list(map(str, s)))
-print(ss)
 
 f = ss.split()
 print(f)
