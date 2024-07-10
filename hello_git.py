64a5319e4045d1f37ee492d106d34c013e54335d
sAnter <sxxnter@gmail.com>
2024-07-10 23:24:48 +0500

    another commit

--- hello_git.py
+++ hello_git.py
@@ -1,2 +1,8 @@
 name = input("Enter your name ")
 print(f"{name}, will you be my friend?")
+
+name = input("Enter your name ")
+print(f"{name}, will you be my friend?")
+
+name = input("Enter your name ")
+print(f"{name}, will you be my friend?")
