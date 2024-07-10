f92e584ffb99bb3b779fa2ce9021f250fe4b3d48
sAnter <sxxnter@gmail.com>
2024-07-10 23:30:32 +0500

    da ne znayu

--- why.py
+++ why.py
@@ -1,6 +1,9 @@
 s = list(map(lambda x: x*x, [1, 2, 3, 4, 5]))
 ss = ' '.join(list(map(str, s)))
 print(ss)
+s = list(map(lambda x: x*x, [1, 2, 3, 4, 5]))
+ss = ' '.join(list(map(str, s)))
+print(ss)
 
 f = ss.split()
 print(f)
